7ce7f569ce97879250a85318cf0d49bd936ceb37
Jose Maria Micoli <josemaria.micoli@gmail.com>
2026-03-13 12:17:49 -0300

    feat: add graceful shutdown and optional TLS to teamserver

--- server/go_backend/cmd/teamserver/main.go
+++ server/go_backend/cmd/teamserver/main.go
@@ -1,29 +1,92 @@
 package main
 
 import (
-	"flag"
-	"log"
+	"context"
+	"crypto/tls"
+	"errors"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
+	"redforgec2/server/go_backend/internal/config"
+	"redforgec2/server/go_backend/internal/events"
 	"redforgec2/server/go_backend/internal/httpapi"
+	"redforgec2/server/go_backend/internal/logging"
 	"redforgec2/server/go_backend/internal/store"
 )
 
 func main() {
-	addr := flag.String("addr", "127.0.0.1:8080", "listen address (lab-only; default binds to localhost)")
-	flag.Parse()
-
-	st := store.New()
-	router := httpapi.NewRouter(st)
-
-	srv := &http.Server{
-		Addr:              *addr,
-		Handler:           router,
-		ReadHeaderTimeout: 5 * time.Second,
+	cfg, err := config.Load(os.Args[1:])
+	if err != nil {
+		_, _ = os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(2)
 	}
 
-	log.Printf("redforgec2 teamserver (simulation) listening on http://%s", *addr)
-	log.Fatal(srv.ListenAndServe())
-}
+	logger, closeLog, err := logging.NewLogger(logging.Options{JSON: cfg.LogJSON, LogFile: cfg.LogFile})
+	if err != nil {
+		_, _ = os.Stderr.WriteString(err.Error() + "\n")
+		os.Exit(2)
+	}
+	defer func() { _ = closeLog() }()
 
+	ev, err := events.New(cfg.EventLog)
+	if err != nil {
+		logger.Error("event_log_init_failed", "err", err, "path", cfg.EventLog)
+		os.Exit(2)
+	}
+	defer func() { _ = ev.Close() }()
+
+	st := store.New()
+	router := httpapi.NewRouter(httpapi.Deps{Store: st, Logger: logger, Events: ev})
+
+	srv := &http.Server{
+		Addr:              cfg.ListenAddr,
+		Handler:           router,
+		ReadHeaderTimeout: cfg.Timeouts.ReadHeader,
+		TLSConfig:         &tls.Config{MinVersion: tls.VersionTLS12},
+	}
+
+	logger.Info("teamserver_listen", "addr", cfg.ListenAddr, "base_url", cfg.BaseURL)
+
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM)
+	defer stop()
+
+	errCh := make(chan error, 1)
+	go func() {
+		if cfg.TLSCert != "" && cfg.TLSKey != "" {
+			errCh <- srv.ListenAndServeTLS(cfg.TLSCert, cfg.TLSKey)
+			return
+		}
+		errCh <- srv.ListenAndServe()
+	}()
+
+	select {
+	case <-ctx.Done():
+		logger.Info("shutdown_signal")
+	case err := <-errCh:
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("server_error", "err", err)
+			os.Exit(1)
+		}
+		return
+	}
+
+	shutdownCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(shutdownCtx); err != nil {
+		logger.Warn("shutdown_failed", "err", err)
+	} else {
+		logger.Info("shutdown_complete")
+	}
+
+	select {
+	case err := <-errCh:
+		if err != nil && !errors.Is(err, http.ErrServerClosed) {
+			logger.Error("server_error", "err", err)
+			os.Exit(1)
+		}
+	default:
+	}
+}
